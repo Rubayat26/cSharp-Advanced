2dcf5ed35c183c722e43a3ff1415f9478fcb3c73
Rubayat26 <rubayat10me@gmail.com>
2022-08-16 15:51:07 -0400

    Added comments on extension methods

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -11,7 +11,11 @@ namespace ExtensionMethods
         static void Main(string[] args)
         {
             string post = "This is supposed to be a very long blog post blah blah blah...";
-            var shortenedPost = post.Shorten(5);
+            var shortenedPost = post.Shorten(5);  //althgough we are applying extension
+                                                  //methods, in the instance of an object 
+                                                  //essentialy they are static methods
+
+            Console.WriteLine(shortenedPost);   
 
             IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
             var max = numbers.Max();
@@ -21,3 +25,14 @@ namespace ExtensionMethods
         }
     }
 }
+
+
+//post. will show the Shorten method as an extension when writing the code,
+//the other methods will have purple cube shoiwing the methods of string class  
+
+
+
+
+
+// in real word you will be using extension methods most of the time
+//instead of creating them 
--- ExtensionMethods/ExtensionMethods/StringExtensions.cs
+++ ExtensionMethods/ExtensionMethods/StringExtensions.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Linq;
 
-namespace System
+namespace System  //namespace System is used so that the extension method remains available
+                  //to all parts of the program as extension method does not work if
+                  //the class that created them is out of scope 
 {
-    public static class StringExtensions
+    public static class StringExtensions //this has to be declared a static class for
+                                         //adding extensions 
     {
-        public static string Shorten(this String str, int numberOfWords)
+        public static string Shorten(this String str, int numberOfWords) //public static string
+                                                                         //is the syntax 
         {
             if (numberOfWords < 0)
                 throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
@@ -18,7 +22,18 @@ namespace System
             if (words.Length <= numberOfWords)
                 return str;
 
-            return string.Join(" ", words.Take(numberOfWords)) + "...";
+           
+
+            return (string.Join(" ", words.Take(numberOfWords)) + "..."); //
         }
     }
 }
+
+
+//The extension method cannot work if the class created them is out of scope
+
+
+
+// string array is an array which does not have Take method. But later microsoft introduced
+//Take method which is an extension method that can be implemented to any class that 
+//implements an IEnumerable Interface, in this case the str array. 
